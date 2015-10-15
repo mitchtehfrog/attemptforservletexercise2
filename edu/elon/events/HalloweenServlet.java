60e326c21f61c3ebc05e37c86c6d96e98d0a931c
unknown <mthompson31@CARPMAC-07.fs.elon.edu>
2015-10-15 16:22:34 -0400

    fixed some logic regarding form actions

--- edu/elon/events/HalloweenServlet.java
+++ edu/elon/events/HalloweenServlet.java
@@ -29,6 +29,7 @@ public class HalloweenServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
@@ -82,7 +83,7 @@ public class HalloweenServlet extends HttpServlet {
             //out.println("</body>");
             //out.println("</html>");
         //}
-    }
+    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -93,11 +94,7 @@ public class HalloweenServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+   
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -107,20 +104,12 @@ public class HalloweenServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
+   
     /**
      * Returns a short description of the servlet.
      *
      * @return a String containing servlet description
      */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+   
 }
+
